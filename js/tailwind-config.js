2b359a7ce36c8bd20d06e593a198ce6b44744dd5
kohjihyun <nakia@icbnet.co.kr>
2026-06-02 13:09:47 +0900

    Add files via upload

---
+++ js/tailwind-config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'icb-orange': '#e52006',
+        'icb-orange-dark': '#ea4109',
+        'icb-orange-bright': '#ea3609',
+        'icb-dark': '#0A1221',
+        'icb-beige': '#f5ece9',
+        'icb-bg': '#f8f3f2',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        sora: ['Sora', 'sans-serif'],
+      },
+      maxWidth: {
+        site: '1440px',
+      },
+    },
+  },
+};
